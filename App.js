81c4628444b27c5c0a8b0ac0dde283e6a7e182a5
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2021-06-11 17:04:11 -0300

    att

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Background from 'assets/img/HomePage.png';
 
 export default function App() {
   return (
@@ -11,11 +12,15 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const styles = StyleSheet.create(
+ 
+  backgroundImage: url( + assets/img/HomePage.png ),
+      backgroundPosition: center,
+      backgroundSize: cover,
+      backgroundRepeat: no-repeat,
+    }
+      
 });
+
+
+
---
+++ Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text, ProgressViewIOSComponent} from 'react-native';
+
+export default function Page(){
+
+    return(
+        <View>
+            
+            
+        </View>
+    );
+
+}

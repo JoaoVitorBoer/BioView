a4c1d26430ab906d3614e6fb4a8cf41917cc8ca4
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2021-06-12 19:38:22 -0300

    att

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Background from 'assets/img/HomePage.png';
+import ScrollableTabsButtonAuto from './Scroll';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollableTabsButtonAuto>
+
+      </ScrollableTabsButtonAuto>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create(
+const styles = StyleSheet.create({
  
-  backgroundImage: url( + assets/img/HomePage.png ),
-      backgroundPosition: center,
-      backgroundSize: cover,
-      backgroundRepeat: no-repeat,
-    }
+  
       
 });
 

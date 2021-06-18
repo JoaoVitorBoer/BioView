1304e6d4c2f3da88146412f236283fadb5fef221
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2021-06-18 14:49:30 -0300

    Revert "Revert "Home Working""
    This reverts commit a1949aaf5f71ba7043fd348986139324a6fbfa87.

--- src/components/TreeButton/index.js
+++ src/components/TreeButton/index.js
@@ -7,22 +7,24 @@ import { Button, Label } from './styles';
 
 export default function TreeButton({ onPress, focused }) {
   return(
+   
   <TouchableWithoutFeedback onPress={ onPress }>
     <Button
      colors={
       focused
-        ? ['#fff', '#ccc']
-        : ['#2CB039', '#123922']
+        ? ['#fff', '#fff' ]
+        : [ '#2CB039','#97DC25']
   }
       
      start={[1, 0.2]}
     >
       <FontAwesome5 
       name="tree" 
-      size={30} 
-      color={focused ? '#000' : '#fff'}
+      size={35} 
+      color={focused ? '#2CB039' : '#fff'}
+      
        />
-      <Label focused >Home</Label>
+     
 
     </Button>
   </TouchableWithoutFeedback>

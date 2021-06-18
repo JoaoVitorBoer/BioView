c0ec7ccd4acede0ab7618beceb78d7fc448d9017
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2021-06-18 17:00:42 -0300

    Header pronta

---
+++ src/screens/Home/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, SafeAreaView, StyleSheet, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function Header(){
+    return(
+        <SafeAreaView>
+            <View style={styles.header}>
+                <Image
+                source={require('./Header.png')}
+                style={{width: 200, height: 40, alignItems: 'center', justifyContent: 'center', marginLeft: 115, marginTop: 10}}
+                />
+
+            </View>
+        </SafeAreaView>
+
+    );
+}
+ 
+
+const styles = StyleSheet.create({
+    header:{
+        backgroundColor: '#fff'
+
+    }
+});
+
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,32 +1,48 @@
 import React from 'react';
 import { View, ImageBackground, StyleSheet } from 'react-native';
+import Header from './header';
+
 
 
 const image = require('./Homepage.png');
 
 export default function Home(){
     return (
-        <View style={style.viewback}> 
-        <ImageBackground source={ image } style={style.background}>
-          
-        </ImageBackground>
-    </View>
+    
+ <ImageBackground source={ image } style={style.background}>
+   <View style={style.viewback}>    
+    <Header>                  
+    </Header>
+  </View>
+ </ImageBackground>
+
+   
     );
 
 }
+
+
+
+
+
+
 
 const style = StyleSheet.create({              
         viewback:{
 
         flex: 1,
         flexDirection: "column",
+        marginBottom: 1
+
     
     },
     
     background: {
         flex: 1,
         resizeMode: "cover",
-        justifyContent: "center"
+        justifyContent: "center",
+        
+        
     
     },
     

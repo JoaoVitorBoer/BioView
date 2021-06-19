51c38733a118edf8f4131c00138a412a1e12ce1f
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2021-06-18 22:23:46 -0300

    Map

--- src/screens/Mapa/index.js
+++ src/screens/Mapa/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, ImageBackground, StyleSheet } from 'react-native';
+import Header from '../Home/header';
 
-export default function Mapa(){
-    return <Text> Mapa </Text>;
+
+
+const image = require('./Map.png');
+
+export default function Home(){
+    return (
+    
+ <ImageBackground source={ image } style={style.background}>
+   <View style={style.viewback}>    
+    <Header>                  
+    </Header>
+  </View>
+ </ImageBackground>
+
+   
+    );
 
 }
+
+
+
+
+
+
+
+const style = StyleSheet.create({              
+        viewback:{
+
+        flex: 1,
+        flexDirection: "column",
+        marginBottom: 1
+
+    
+    },
+    
+    background: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        
+        
+    
+    },
+    
+    
+    
+    });
+    
+

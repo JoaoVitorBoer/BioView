ccf7d4bd9b1a0344eeea7decc9b7d27255f0e45a
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2021-07-06 16:09:03 -0300

    qrcode

--- src/screens/Qrcode/index.js
+++ src/screens/Qrcode/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+import { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import {css} from '../../../assets/css/Css';
 
 export default function Qrcode(){
-    return <Text> Qrcode </Text>;
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [displayQR, setDisplayQR] = useState('flex');
 
+    useEffect(() => {
+        (async () => {
+          const { status } = await BarCodeScanner.requestPermissionsAsync();
+          setHasPermission(status === 'granted');
+        })();
+      }, []);
+
+      async function handleBarCodeScanned({ type, data }){
+        setScanned(true);
+        setScanned(true);
+        setDisplayQR('none');
+        setDisplayForm('flex');
+        setCode(data);
+ 
+      };
+      return (
+        <View>
+        <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : value=>handleBarCodeScanned(value)}
+        style={css.code(displayQR)}
+        />
+       </View>
+      );
 }
